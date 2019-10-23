a461cce2c25912f4d22f70399d138115912ec0ac
lixn <2505300411@qq.com>
2019-10-23 09:19:42 +0800

    [CppPrimer][10] 4-3 instructions reverse_iterator
    how to use rbegin() and rend() function
    
    Signed-off-by: lixn <2505300411@qq.com>

--- CppPrimer/10/4_3/tk10_37.cpp
+++ CppPrimer/10/4_3/tk10_37.cpp
@@ -2,6 +2,7 @@
 #include <iterator>
 #include <algorithm>
 #include <list>
+#include <vector>
 
 using namespace std;
 
@@ -9,15 +10,18 @@ int main()
 {
 	vector<int> vecVal;
 	istream_iterator<int> in(cin), eof;
+	ostream_iterator<int> out(cout, " ");
 	list<int> lVal;
 
 	copy(in, eof, back_inserter(vecVal));
-	auto vecVal3 = vecVal.rend() - 3;
-	auto vecVal7 = vecVal.rbegin() + 2;
+	auto vecVal3 = vecVal.rend() - 2;
+	auto vecVal7 = vecVal.rbegin() + 3;
 	for(; vecVal7 != vecVal3; vecVal7++)
 	{
 		lVal.push_back(*vecVal7);
 	}
+	
+	copy(lVal.begin(), lVal.end(), out);
 
 	return 0;
 }
